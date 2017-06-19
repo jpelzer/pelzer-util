dcee324cccc6012044174167a84be5f717fecd9d
Jason Pelzer <github.com@pelzer.com>
2017-06-18 22:53:49 -0400

    Changed deploy mvn command

--- deploy-to-central.sh
+++ deploy-to-central.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn clean test javadoc:jar source:jar package org.apache.maven.plugins:maven-gpg-plugin:sign deploy
-
+#mvn clean test javadoc:jar source:jar package org.apache.maven.plugins:maven-gpg-plugin:sign deploy
+mvn clean release:clean release:prepare release:perform -e
