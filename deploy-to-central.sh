62cdf22c50f1c159e9da04793337fa8746b97e60
Jason Pelzer <jason@pelzer.com>
2017-06-23 15:58:12 -0400

    Fixed Log.gdsl and improved deploy script.

--- deploy-to-central.sh
+++ deploy-to-central.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export GPG_TTY=$(tty)
 #mvn clean test javadoc:jar source:jar package org.apache.maven.plugins:maven-gpg-plugin:sign deploy
 mvn clean release:clean release:prepare release:perform -e
